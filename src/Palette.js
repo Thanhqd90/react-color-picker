c809ef307d913c1b73ee7c2b3cffc6fd27141bfb
Thanh Dang <thanhqd1990@gmail.com>
2019-06-03 13:15:41 -0500

    Added snackbar and footer

--- src/Palette.js
+++ src/Palette.js
@@ -2,27 +2,39 @@ import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import "./Palette.css";
+import color from "@material-ui/core/colors/teal";
 
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
   changeLevel(level) {
     this.setState({ level });
   }
+  changeFormat(val) {
+    this.setState({ format: val });
+  }
   render() {
-    const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { colors, paletteName, emoji } = this.props.palette;
+    const { level, format } = this.state;
     const colorBoxes = colors[level].map(c => (
-      <ColorBox background={c.hex} name={c.name} />
+      <ColorBox background={c[format]} name={c.name} key={color.id} />
     ));
     return (
       <div className="Palette">
-        <Navbar level={level} changeLevel={this.changeLevel}/>
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className="Palette-colors">{colorBoxes}</div>
-        {/* Footer goes here */}
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
