ca9aa8b4f83029674ae4f64a12bcfa933d5638b4
Thanh Dang <thanh_dang@qbsol.com>
2019-08-29 09:46:29 -0500

    Updates

---
+++ precache-manifest.2c5a46f371c1ae1c9404aa5de81391cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9931bbe99dc9d750d8d4375557c2c161",
+    "url": "/react-color-picker/index.html"
+  },
+  {
+    "revision": "fffaf72d8b9285affb1e",
+    "url": "/react-color-picker/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "61d6c7db561bbfe584af",
+    "url": "/react-color-picker/static/css/main.13046120.chunk.css"
+  },
+  {
+    "revision": "fffaf72d8b9285affb1e",
+    "url": "/react-color-picker/static/js/2.ec97d6d8.chunk.js"
+  },
+  {
+    "revision": "61d6c7db561bbfe584af",
+    "url": "/react-color-picker/static/js/main.02d9b312.chunk.js"
+  },
+  {
+    "revision": "021ed8086a2f9d9156cb",
+    "url": "/react-color-picker/static/js/runtime~main.a264689d.js"
+  }
+]);
